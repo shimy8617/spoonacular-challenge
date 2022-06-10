b9d2ea28c644f8a2187f9567656d63d5ac7f9e9b
shimy8617 <cristinashim@yahoo.com.ar>
2022-06-10 20:36:30 -0300

    me

--- spoonacular/src/components/views/Login/Login.jsx
+++ spoonacular/src/components/views/Login/Login.jsx
@@ -50,39 +50,39 @@ export const Login = () => {
 
   const { handleSubmit, handleChange, values, errors } = formik;
 
-  let token = sessionStorage.getItem("token");
+  /* let token = sessionStorage.getItem("token"); */
 
   return (
     <>
       {/*{token && <Redirect to="/menu" />}*/}
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <h1>Iniciar sesión</h1>
-        <div>
-          <label>Email</label>
-          <input
-            name="email"
-            type="email"
-            value={values.email}
-            onChange={handleChange}
-          />
-          {errors.email && <div>{errors.email}</div>}
-        </div>
-        <div>
-          <label>Contraseña</label>
-          <input
-            name="password"
-            type="password"
-            value={values.password}
-            onChange={handleChange}
-          />
-          {errors.password && <div>{errors.password}</div>}
-        </div>
-        <div>
-          <button type="submit">Enviar</button>
-        </div>
-      </form>
-    </div>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <h1>Iniciar sesión</h1>
+          <div>
+            <label>Email</label>
+            <input
+              name="email"
+              type="email"
+              value={values.email}
+              onChange={handleChange}
+            />
+            {errors.email && <div>{errors.email}</div>}
+          </div>
+          <div>
+            <label>Contraseña</label>
+            <input
+              name="password"
+              type="password"
+              value={values.password}
+              onChange={handleChange}
+            />
+            {errors.password && <div>{errors.password}</div>}
+          </div>
+          <div>
+            <button type="submit">Enviar</button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
